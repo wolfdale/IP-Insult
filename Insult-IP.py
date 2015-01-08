99ad4292f7a876e17e94c936bc6c41bd4757cb59
Prakhar Gupta <mywolfdale@gmail.com>
2015-01-08 21:38:27 +0530

    Update Insult-IP.py
    Comments !

--- Insult-IP.py
+++ Insult-IP.py
@@ -1,48 +1,56 @@
+################################
+# IP Insult Version 0.0.1 
+# Author - Prakhar (Wolfdale)
+# For IPv4 Only
+################################
+
 def main():
         import re
         import urllib
-        web=urllib.urlopen('http://ip-lookup.net/')
+        web=urllib.urlopen('http://ip-lookup.net/')  #Getting IP Adress 
         content=web.read()
         m = re.search('(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)',
-                content)
+                content)    # RegEx for Ip Adress Lookup in Web page          
         myip = m.group(0)
-        #print myip
+        #print myip         [BreakPoint 1]
         if len(myip)<15:
                 myip=parse_ip(myip)
-        #print 'Ready To Go !'
+        #print 'Ready To Go !' [BreakPoint 2]
         print myip
-        #raw_input()
+        #raw_input()   [BreakPoint 3]
         setip(myip)
-
-def parse_ip(ip):
+#Parsing Ip Adress (Full Representation)
+def parse_ip(ip):                                          
         p1,p2,p3,p4=ip.split('.')      
-        def check(p):
-                if len(p)<3:
+        def check(p):                                    # Avoiding For Loop; All hail If condition !
+                if len(p)<3:                                  
                         if len(p)<2:
                                 p='0'+p
                         p='0'+p
-                return p        
-        p1=check(p1)
+                return p
+        #Checking Each quad of Ip Address        
+        p1=check(p1) 
         p2=check(p2)
         p3=check(p3)
         p4=check(p4)
-        #print p1,p2,p3,p4
+        #print p1,p2,p3,p4   [BreakPoint 4]
         str='.'
         seq=[p1,p2,p3,p4]
         return (str.join(seq))
                        
 def setip( ip ):
+	# Min-Max Normalization to map high range to low range 
 	q1=int((str(ip[0])) + (str(ip[1])) + (str(ip[2])) )
 	q2=int((str(ip[4])) + (str(ip[5])) + (str(ip[6])))
 	q3=int((str(ip[8])) + (str(ip[9])) + (str(ip[10])) )
 	q4=int((str(ip[12])) + (str(ip[13])) + (str(ip[14])) )
-	#print q1,q2,q3,q4
+	#print q1,q2,q3,q4 [BreakPoint 5]
         s=int((((q1-0)/255.0)*44))
 	t=int((((q2-0)/255.0)*44))
 	u=int((((q3-0)/255.0)*44))
 	v=int((((q4-0)/255.0)*44))
-	#print s,t,u,v
-	#raw_input()
+	#print s,t,u,v [BreakPoint 6]
+	#raw_input()   [BreakPoint 5]
 	display(s,t,u,v)
 
 def display (s,t,u,v):
@@ -110,7 +118,7 @@ def display (s,t,u,v):
         ,'fishified'      ,     'leaden-footed'     ,  'rascal'
         ,'Wart-necked'     ,    'muddy-mettled'      , 'Basket-Cockle'];
         print ('Thou'+ ' ' + col1[s] + ' ' + col2[t] +' '+ col3[u] +' '+ col4[v] + ' ' + '!' )
-        raw_input()
+        raw_input() 
 
 if __name__ == "__main__":
 	main()
